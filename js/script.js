56613599fd1425358841263ba8be564ea2ea8a95
shubham <shubham.bhoyar077@gmail.com>
2023-01-30 17:59:40 +0530

    Fix js linter error

--- js/script.js
+++ js/script.js
@@ -1,18 +1,18 @@
-let openbtn = document.querySelector(".nav-bar .toggle-menu");
-let mobileMenu = document.querySelector(".nav-bar .mobile-menu");
-let menuBar = document.querySelector(".nav-bar .menu-bar");
-let closebtn = document.querySelector(".nav-bar .close-menu");
-let navLinks = document.querySelector(".nav-bar .mobile-menu .nav-links")
+const openbtn = document.querySelector('.nav-bar .toggle-menu');
+const mobileMenu = document.querySelector('.nav-bar .mobile-menu');
+const menuBar = document.querySelector('.nav-bar .menu-bar');
+const closebtn = document.querySelector('.nav-bar .close-menu');
+const navLinks = document.querySelector('.nav-bar .mobile-menu .nav-links');
 
-function toggleMenu(){
-    mobileMenu.classList.toggle('disable');
-    menuBar.classList.toggle('disable');
+function toggleMenu() {
+  mobileMenu.classList.toggle('disable');
+  menuBar.classList.toggle('disable');
 }
 
 openbtn.addEventListener('click', toggleMenu);
 closebtn.addEventListener('click', toggleMenu);
-navLinks.addEventListener('click', function(e){
-    if(e.target.className === "nav-item"){
-        toggleMenu()
-    }
+navLinks.addEventListener('click', (e) => {
+  if (e.target.className === 'nav-item') {
+    toggleMenu();
+  }
 });
