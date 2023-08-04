4f074508751b7d18bda8b8d90fd91d359fac5411
shubham <shubham.bhoyar077@gmail.com>
2023-08-04 21:18:26 +0530

    Add typing ani

--- script.js
+++ script.js
@@ -1,25 +1,43 @@
-const date = document.getElementById('date')
+const date = document.getElementById('date');
 const setCurrentYear = () => {
-	const currentYear = new Date().getFullYear()
-	date.textContent = currentYear
-}
-setCurrentYear()
+  const currentYear = new Date().getFullYear();
+  date.textContent = currentYear;
+};
+setCurrentYear();
 
 // hamburger menu
-const hamburger = document.querySelector('.hamburger')
-const navList = document.querySelector('.nav-list')
-const socials = document.querySelector('.social-nav')
-const navLinks = document.querySelectorAll('.nav-links')
+const hamburger = document.querySelector('.hamburger');
+const navList = document.querySelector('.nav-list');
+const socials = document.querySelector('.social-nav');
+const navLinks = document.querySelectorAll('.nav-links');
 
 const toggleMobileMenu = () => {
-	hamburger.classList.toggle('open')
-	navList.classList.toggle('open')
-	socials.classList.toggle('open')
-	document.body.classList.toggle('open')
-}
+  hamburger.classList.toggle('open');
+  navList.classList.toggle('open');
+  socials.classList.toggle('open');
+  document.body.classList.toggle('open');
+};
 
-navLinks.forEach(link => link.addEventListener('click', toggleMobileMenu))
-hamburger.addEventListener('click', toggleMobileMenu)
+navLinks.forEach((link) => link.addEventListener('click', toggleMobileMenu));
+hamburger.addEventListener('click', toggleMobileMenu);
 
 // initialize aos (library for scroll animation)
-AOS.init()
+AOS.init();
+
+const professions = [
+  'Software Developer',
+  'Backend Developer',
+  'Frontend Developer',
+  'Full Stack Developer',
+];
+
+const professionElement = document.querySelector('.service span');
+if (professionElement) {
+  new Typed('.service span', {
+    strings: professions,
+    loop: true,
+    typeSpeed: 100,
+    backSpeed: 50,
+    backDelay: 2000,
+  });
+}
