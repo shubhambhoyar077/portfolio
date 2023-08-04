29939526fc4063377e3a19e0727ece59742fd989
shubham <shubham.bhoyar077@gmail.com>
2023-08-04 18:56:33 +0530

    using build in portfolio

---
+++ script.js
@@ -0,0 +1,25 @@
+const date = document.getElementById('date')
+const setCurrentYear = () => {
+	const currentYear = new Date().getFullYear()
+	date.textContent = currentYear
+}
+setCurrentYear()
+
+// hamburger menu
+const hamburger = document.querySelector('.hamburger')
+const navList = document.querySelector('.nav-list')
+const socials = document.querySelector('.social-nav')
+const navLinks = document.querySelectorAll('.nav-links')
+
+const toggleMobileMenu = () => {
+	hamburger.classList.toggle('open')
+	navList.classList.toggle('open')
+	socials.classList.toggle('open')
+	document.body.classList.toggle('open')
+}
+
+navLinks.forEach(link => link.addEventListener('click', toggleMobileMenu))
+hamburger.addEventListener('click', toggleMobileMenu)
+
+// initialize aos (library for scroll animation)
+AOS.init()
